8a49d424c8359ff9169f1e08bf86f428b6861eb6
jaredmartin5 <jaredm776@cox.net>
2021-08-25 21:19:18 -0500

    updated to 13.1 and made a button

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,19 @@
+const { MessageActionRow, MessageButton } = require('discord.js');
+
 module.exports = {
 	name: 'ping',
 	description: 'Replies with Pong!',
-	async execute(interactionCreate) {
-		return interactionCreate.reply('Pong!');
+	async execute(interaction) {
+		if (interaction.commandName === 'ping') {
+			const row = new MessageActionRow()
+				.addComponents(
+					new MessageButton()
+						.setLabel('CLICK ME!')
+						.setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+						.setStyle('LINK'),
+				);
+	
+			await interaction.reply({ content: 'Pong!', components: [row] });
+		}
 	},
 };
