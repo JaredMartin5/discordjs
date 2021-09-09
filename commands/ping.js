20392a63fb7e4d7fa797ebeec5d43fc1f032bd5c
jaredmartin5 <jaredm776@cox.net>
2021-09-09 15:57:49 -0500

    forgot to stage ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,19 +1,25 @@
-const { MessageActionRow, MessageButton } = require('discord.js');
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 
 module.exports = {
 	name: 'ping',
 	description: 'Replies with Pong!',
 	async execute(interaction) {
-		if (interaction.commandName === 'ping') {
-			const row = new MessageActionRow()
-				.addComponents(
-					new MessageButton()
-						.setLabel('CLICK ME!')
-						.setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-						.setStyle('LINK'),
-				);
-	
-			await interaction.reply({ content: 'Pong!', components: [row] });
-		}
+
+		const row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+					.setLabel('CLICK ME!')
+					.setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+					.setStyle('LINK')
+			);
+
+		const embed = new MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle('CLICK THIS TITLE!')
+			.setURL('https://popcat.click/')
+			.setDescription('Just click the title to see the video.');
+
+		await interaction.reply({ content: 'Pong!', ephemeral: false, embeds: [embed], components: [row] });
+
 	},
 };
